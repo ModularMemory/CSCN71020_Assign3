0e7a77fc7e4a71159db8069d36dc0f180170af15
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-10-31 16:40:18 -0400

    Init

---
+++ RPS Lib.Tests/RPS Lib.Tests.cpp
@@ -0,0 +1,19 @@
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "../RPS Lib/rps.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RPSLibTests
+{
+	TEST_CLASS(RPSLibTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
