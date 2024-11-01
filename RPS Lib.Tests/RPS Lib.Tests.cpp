7ba79dba7a306b2dc4ce7f6034307dc6c11bc48f
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-11-01 15:26:31 -0400

    Test invalid player 1 & player 2 input

--- RPS Lib.Tests/RPS Lib.Tests.cpp
+++ RPS Lib.Tests/RPS Lib.Tests.cpp
@@ -152,5 +152,17 @@ namespace RPSLibTests
 
             Assert::AreEqual(EXPECTED, actual);
         }
+
+        TEST_METHOD(Invalid_WhenBothPlayersInvalidInput)
+        {
+            const char* PLAYER_1_INPUT = "Foo";
+            const char* PLAYER_2_INPUT = "Bar";
+            const char* EXPECTED = INVALID_INPUT;
+
+            char actual[RESULT_MAX_LENGTH] = { 0 };
+            runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
+
+            Assert::AreEqual(EXPECTED, actual);
+        }
     };
 }
