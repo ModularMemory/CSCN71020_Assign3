109aad7867467ffff66db74d7164967b26c94929
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-11-01 15:05:25 -0400

    Rock vs scissors

--- RPS Lib/rps.c
+++ RPS Lib/rps.c
@@ -2,11 +2,23 @@
 
 #include <string.h>
 
-#define PLAYER1_WINS "Player1"
-#define PLAYER2_WINS "Player2"
+#define ROCK "Rock"
+#define PAPER "Paper"
+#define SCISSORS "Scissors"
+
+#define PLAYER_1_WINS "Player1"
+#define PLAYER_2_WINS "Player2"
 #define DRAW "Draw"
 #define INVALID_INPUT "Invalid"
 
 void runRps(const char* player1Input, const char* player2Input, char result[RESULT_MAX_LENGTH]) {
-    
+    char* resultString = INVALID_INPUT;
+
+    if (strcmp(player1Input, ROCK) == 0) {
+        if (strcmp(player2Input, SCISSORS) == 0) {
+            resultString = PLAYER_1_WINS;
+        }
+    }
+
+    strncpy_s(result, RESULT_MAX_LENGTH * sizeof(char), resultString, strlen(resultString));
 }
