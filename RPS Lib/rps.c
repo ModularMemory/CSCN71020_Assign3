10adcba89039417b9ae1ddcc9fc3efb7d5c092fe
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-11-01 20:53:30 -0400

    Update comment

--- RPS Lib/rps.c
+++ RPS Lib/rps.c
@@ -60,6 +60,6 @@ void rockPaperScissors(const char* player1Input, const char* player2Input, char 
         }
     }
 
-    // Copy resultString to result array
+    // Copy contents of resultString to result array
     strncpy_s(result, RESULT_MAX_LENGTH * sizeof(char), resultString, strlen(resultString));
 }
