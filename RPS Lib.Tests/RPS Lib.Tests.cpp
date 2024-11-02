58963d197eae4a4bc9003a0d68545af70986098d
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-11-01 20:43:11 -0400

    Overly explain logic with comments

--- RPS Lib.Tests/RPS Lib.Tests.cpp
+++ RPS Lib.Tests/RPS Lib.Tests.cpp
@@ -4,10 +4,12 @@ extern "C" {
 #include "../RPS Lib/rps.h"
 }
 
+// Input constants
 constexpr auto ROCK = "Rock";
 constexpr auto PAPER = "Paper";
 constexpr auto SCISSORS = "Scissors";
 
+// Result constants
 constexpr auto PLAYER_1_WINS = "Player1";
 constexpr auto PLAYER_2_WINS = "Player2";
 constexpr auto DRAW = "Draw";
@@ -23,145 +25,181 @@ namespace RPSLibTests
         
         TEST_METHOD(Draw_WhenRockVsRock)
         {
+            // Arrange
             const char* PLAYER_1_INPUT = ROCK;
             const char* PLAYER_2_INPUT = ROCK;
             const char* EXPECTED = DRAW;
 
+            // Act
             char actual[RESULT_MAX_LENGTH] = { 0 };
             runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
 
+            // Assert
             Assert::AreEqual(EXPECTED, actual);
         }
 
         TEST_METHOD(Player2Wins_WhenRockVsPaper)
         {
+            // Arrange
             const char* PLAYER_1_INPUT = ROCK;
             const char* PLAYER_2_INPUT = PAPER;
             const char* EXPECTED = PLAYER_2_WINS;
 
+            // Act
             char actual[RESULT_MAX_LENGTH] = { 0 };
             runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
 
+            // Assert
             Assert::AreEqual(EXPECTED, actual);
         }
 
         TEST_METHOD(Player1Wins_WhenRockVsScissors)
         {
+            // Arrange
             const char* PLAYER_1_INPUT = ROCK;
             const char* PLAYER_2_INPUT = SCISSORS;
             const char* EXPECTED = PLAYER_1_WINS;
 
+            // Act
             char actual[RESULT_MAX_LENGTH] = { 0 };
             runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
 
+            // Assert
             Assert::AreEqual(EXPECTED, actual);
         }
 
         TEST_METHOD(Player1Wins_WhenPaperVsRock)
         {
+            // Arrange
             const char* PLAYER_1_INPUT = PAPER;
             const char* PLAYER_2_INPUT = ROCK;
             const char* EXPECTED = PLAYER_1_WINS;
 
+            // Act
             char actual[RESULT_MAX_LENGTH] = { 0 };
             runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
 
+            // Assert
             Assert::AreEqual(EXPECTED, actual);
         }
 
         TEST_METHOD(Draw_WhenPaperVsPaper)
         {
+            // Arrange
             const char* PLAYER_1_INPUT = PAPER;
             const char* PLAYER_2_INPUT = PAPER;
             const char* EXPECTED = DRAW;
 
+            // Act
             char actual[RESULT_MAX_LENGTH] = { 0 };
             runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
 
+            // Assert
             Assert::AreEqual(EXPECTED, actual);
         }
 
         TEST_METHOD(Player2Wins_WhenPaperVsScissors)
         {
+            // Arrange
             const char* PLAYER_1_INPUT = PAPER;
             const char* PLAYER_2_INPUT = SCISSORS;
             const char* EXPECTED = PLAYER_2_WINS;
 
+            // Act
             char actual[RESULT_MAX_LENGTH] = { 0 };
             runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
 
+            // Assert
             Assert::AreEqual(EXPECTED, actual);
         }
 
         TEST_METHOD(Player2Wins_WhenScissorsVsRock)
         {
+            // Arrange
             const char* PLAYER_1_INPUT = SCISSORS;
             const char* PLAYER_2_INPUT = ROCK;
             const char* EXPECTED = PLAYER_2_WINS;
 
+            // Act
             char actual[RESULT_MAX_LENGTH] = { 0 };
             runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
 
+            // Assert
             Assert::AreEqual(EXPECTED, actual);
         }
 
         TEST_METHOD(Player1Wins_WhenScissorsVsPaper)
         {
+            // Arrange
             const char* PLAYER_1_INPUT = SCISSORS;
             const char* PLAYER_2_INPUT = PAPER;
             const char* EXPECTED = PLAYER_1_WINS;
 
+            // Act
             char actual[RESULT_MAX_LENGTH] = { 0 };
             runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
 
+            // Assert
             Assert::AreEqual(EXPECTED, actual);
         }
 
         TEST_METHOD(Draw_WhenScissorsVsScissors)
         {
+            // Arrange
             const char* PLAYER_1_INPUT = SCISSORS;
             const char* PLAYER_2_INPUT = SCISSORS;
             const char* EXPECTED = DRAW;
 
+            // Act
             char actual[RESULT_MAX_LENGTH] = { 0 };
             runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
 
+            // Assert
             Assert::AreEqual(EXPECTED, actual);
         }
 
         TEST_METHOD(Invalid_WhenInvalidPlayer1Input)
         {
+            // Arrange
             const char* PLAYER_1_INPUT = "Bar";
             const char* PLAYER_2_INPUT = ROCK;
             const char* EXPECTED = INVALID_INPUT;
 
+            // Act
             char actual[RESULT_MAX_LENGTH] = { 0 };
             runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
 
+            // Assert
             Assert::AreEqual(EXPECTED, actual);
         }
 
         TEST_METHOD(Invalid_WhenInvalidPlayer2Input)
         {
+            // Arrange
             const char* PLAYER_1_INPUT = ROCK;
             const char* PLAYER_2_INPUT = "Foo";
             const char* EXPECTED = INVALID_INPUT;
 
+            // Act
             char actual[RESULT_MAX_LENGTH] = { 0 };
             runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
 
+            // Assert
             Assert::AreEqual(EXPECTED, actual);
         }
 
         TEST_METHOD(Invalid_WhenBothPlayersInvalidInput)
         {
+            // Arrange
             const char* PLAYER_1_INPUT = "Foo";
             const char* PLAYER_2_INPUT = "Bar";
             const char* EXPECTED = INVALID_INPUT;
 
+            // Act
             char actual[RESULT_MAX_LENGTH] = { 0 };
             runRps(PLAYER_1_INPUT, PLAYER_2_INPUT, actual);
 
+            // Assert
             Assert::AreEqual(EXPECTED, actual);
         }
     };
--- RPS Lib/rps.c
+++ RPS Lib/rps.c
@@ -2,51 +2,64 @@
 
 #include <string.h>
 
+// Input constants
 #define ROCK "Rock"
 #define PAPER "Paper"
 #define SCISSORS "Scissors"
 
+// Result constants
 #define PLAYER_1_WINS "Player1"
 #define PLAYER_2_WINS "Player2"
 #define DRAW "Draw"
 #define INVALID_INPUT "Invalid"
 
 void runRps(const char* player1Input, const char* player2Input, char result[RESULT_MAX_LENGTH]) {
+    // Initialize with invalid input message by default
     char* resultString = INVALID_INPUT;
 
     if (strcmp(player1Input, ROCK) == 0) {
+        // Rock vs rock - draw
         if (strcmp(player2Input, ROCK) == 0) {
             resultString = DRAW;
         }
+        // Rock vs paper - paper
         else if (strcmp(player2Input, PAPER) == 0) {
             resultString = PLAYER_2_WINS;
         }
+        // Rock vs scissors - rock
         else if (strcmp(player2Input, SCISSORS) == 0) {
             resultString = PLAYER_1_WINS;
         }
     }
     else if (strcmp(player1Input, PAPER) == 0) {
+        // Paper vs rock - paper
         if (strcmp(player2Input, ROCK) == 0) {
             resultString = PLAYER_1_WINS;
         }
+        // Paper vs paper - draw
         else if (strcmp(player2Input, PAPER) == 0) {
             resultString = DRAW;
         }
+        // Paper vs scissors - scissors
         else if (strcmp(player2Input, SCISSORS) == 0) {
             resultString = PLAYER_2_WINS;
         }
     }
     else if (strcmp(player1Input, SCISSORS) == 0) {
+        // Scissors vs rock - rock
         if (strcmp(player2Input, ROCK) == 0) {
             resultString = PLAYER_2_WINS;
         }
+        // Scissors vs paper - scissors
         else if (strcmp(player2Input, PAPER) == 0) {
             resultString = PLAYER_1_WINS;
         }
+        // Scissors vs scissors - draw
         else if (strcmp(player2Input, SCISSORS) == 0) {
             resultString = DRAW;
         }
     }
 
+    // Copy resultString to result array
     strncpy_s(result, RESULT_MAX_LENGTH * sizeof(char), resultString, strlen(resultString));
 }
